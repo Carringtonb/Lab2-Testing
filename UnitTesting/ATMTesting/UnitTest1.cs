3788657864a098d8a284b3c5dc4378acc74e6780
carrington <carringtonbeard@live.com>
2020-03-18 13:02:30 -0700

    added lab2

---
+++ UnitTesting/ATMTesting/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+using UnitTesting;
+
+namespace ATMTesting
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Program.balance = 500;
+            Program.Withdrawal(420);
+            Assert.Equal(80, Program.balance);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Program.balance = 500;
+            Program.Withdrawal(520);
+            Assert.Equal(-20, Program.balance);
+        }
+    }
+
+      
+}
